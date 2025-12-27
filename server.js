2357d39493066c0f1591d2a096c25baea28a5bed
mrtwoskiii-dotcom <mrtwoskiii@gmail.com>
2025-12-27 16:59:48 +0000

    refreshed

--- server.js
+++ server.js
@@ -1,10 +1,7 @@
 const express = require('express');
 const app = express();
 const PORT = 3000;
-
-// Serve the public folder
 app.use(express.static('public'));
-
 app.listen(PORT, () => {
-    console.log(`Free InkVision running on port ${PORT}`);
+    console.log(`InkVision Designer & AI running on port ${PORT}`);
 });
